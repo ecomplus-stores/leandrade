5d5a539028bd9afb48005bc6d9a3546407745709
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-02-10 15:46:37 -0300

    add: mega menu and change price order

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html')
+      //'./html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html')
     }
   }
 })
